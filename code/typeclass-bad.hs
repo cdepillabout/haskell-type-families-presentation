fdd2dff3cf8e31cd0b82966d522f768625f412b8
(cdep)illabout <cdep.illabout@gmail.com>
2014-04-26 17:37:45 +0900

    Update

--- code/typeclass-bad.hs
+++ code/typeclass-bad.hs
@@ -1,21 +1,16 @@
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TypeFamilies #-}
 
 class Add a b where
-    type SumTy a b
-    plus :: a -> b -> SumTy a b
+    plus :: a -> b -> c
 
 instance Add Integer Double where
-    type SumTy Integer Double = Double
     plus x y = fromIntegral x + y
 
 instance Add Double Integer where
-    type SumTy Double Integer = Double
     plus x y = x + fromIntegral y
 
-instance (Num a) => Add a a where
-    type SumTy a a = a
-    plus x y = x + y
-
 main :: IO ()
-main = putStrLn "Hello"
+main = do
+        print $ plus (5 :: Integer) 6
+        print $ plus (5.0 :: Double) 6.0
 
--- code/typeclass.hs
+++ code/typeclass.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TypeFamilies #-}
 
 class Add a where
-    add :: a -> a -> a
+    plus :: a -> a -> a
 
 instance Add Integer where
-    add x y = x + y
+    plus x y = x + y
 
 instance Add Double where
-    add x y = x + y
+    plus x y = x + y
 
 main :: IO ()
 main = do
-        print $ add (5 :: Integer) 6
-        print $ add (5.0 :: Double) 6.0
+        print $ plus (5 :: Integer) 6
+        print $ plus (5.0 :: Double) 6.0
